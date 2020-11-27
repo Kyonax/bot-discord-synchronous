020f4813888909fa290041c28cfa9f9a16a120f6
Kyonax <kyonax25@gmail.com>
2020-11-26 23:24:19 -0500

    Fixing Connection

--- database/mysql/db.js
+++ database/mysql/db.js
@@ -9,5 +9,8 @@ module.exports = mysql
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
   })
   .catch((err) => console.log(err));
