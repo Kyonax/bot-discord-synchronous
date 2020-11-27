530c6b57ee65554620944ab8b3f2c549105a5f6f
Kyonax <kyonax25@gmail.com>
2020-11-26 23:46:51 -0500

    Testing something

--- database/mysql/db.js
+++ database/mysql/db.js
@@ -2,41 +2,13 @@
 require("dotenv").config();
 const mysql = require("mysql2/promise");
 //Exportando Conexión con la Base de Datos
-var db_config = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-};
-
-var connection;
-
-module.exports = mysql.createConnection(db_config);
-
-function handleDisconnect() {
-  console.log("1. connecting to db:");
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-  // the old one cannot be reused.
-
-  connection.connect(function (err) {
-    // The server is either down
-    if (err) {
-      // or restarting (takes a while sometimes).
-      console.log("2. error when connecting to db:", err);
-      setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }); // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
-  connection.on("error", function (err) {
-    console.log("3. db error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
-    } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
-    }
-  });
-}
-
-
+module.exports = mysql
+  //Creando Conexión con la base de Datos usando dotenv
+  .createConnection({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME    
+  })
+  .catch((err) => console.log(err));
